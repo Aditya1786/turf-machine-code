ea38897488ebe2d9f53266c1bc6aa3431fb8bc80
Aditya Singh <adityasingh.dev.1786@gmail.com>
2025-10-18 12:40:17 +0530

    fixed runnable

--- src/main/java/com/aditya/TurfApplication.java
+++ src/main/java/com/aditya/TurfApplication.java
@@ -1,18 +1,21 @@
 package com.aditya;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication(scanBasePackages = {"com.aditya"})
 @EnableAspectJAutoProxy
-public class TurfApplication implements Runnable {
+public class TurfApplication implements CommandLineRunner {
   public static void main(String[] args) {
     SpringApplication.run(TurfApplication.class, args);
   }
 
   @Override
-  public void run() {
+  public void run(String... args) {
+    // log server logs
     System.out.println("Turf Application Started");
+
   }
 }
